dc25ed4ae45fdefa2132a51971c3aafa224a2673
itsyagirlV <154456371+itsyagirlLavender007@users.noreply.github.com>
2024-09-20 19:33:51 +0530

    index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const app = express();
 const port = 3000;
 
-mongoose.connect("mongodb://localhost:27017/",{
+mongoose.connect("connections-name",{
     dbName:"userQuotes",
 }).then(()=>console.log("Database connected"))
 .catch((err)=>console.log(err));
